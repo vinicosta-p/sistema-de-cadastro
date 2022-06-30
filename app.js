05bd52523bfeb85c8c786294a82314328a30a136
Edgar Almeida <edgar.almeida@flexsimbrasil.com.br>
2022-06-30 12:23:30 -0300

    deu ruim! bora voltar

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("rodando sistema de gerenciamento de jogos")
 console.log("timmy é um garoto bom")
-// mais um console log de teste:
-console.log("O paulo fez esse commit sem voce ver na aula!")
